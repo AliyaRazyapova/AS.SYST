b8cf65455098c02c61f2a8ecbe9453aa483c1ebe
Razyapova Aliya <a.raziapova@mail.ru>
2023-02-25 23:22:58 +0300

    done computer version

---
+++ main/script.js
@@ -0,0 +1,41 @@
+function openUrlPopup() {
+    var url = prompt("Введите URL-адрес");
+    if (url) {
+      var textArea = document.getElementById("text");
+      textArea.value += url;
+    }
+  }
+
+function toggleBold() {
+    var textBox = document.getElementById("text");
+    if (textBox.style.fontWeight === "bold") {
+        textBox.style.fontWeight = "normal";
+    } else {
+        textBox.style.fontWeight = "bold";
+    }
+}
+
+function toggleUnderline() {
+  var input = document.getElementById("text");
+  var currentStyle = input.style.textDecoration;
+  if (currentStyle === "underline") {
+    input.style.textDecoration = "none";
+  } else {
+    input.style.textDecoration = "underline";
+  }
+}
+
+function toggleItalic() {
+  var input = document.getElementById("text");
+  var currentStyle = input.style.fontStyle;
+  if (currentStyle === "italic") {
+    input.style.fontStyle = "normal";
+  } else {
+    input.style.fontStyle = "italic";
+  }
+}
+
+function addBulletPoint() {
+  var textarea = document.getElementById("text");
+  textarea.value += "\n• ";
+}
