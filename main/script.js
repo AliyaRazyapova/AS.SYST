fdca21d7c311767f86aba1b77a66c7648670297f
Razyapova Aliya <a.raziapova@mail.ru>
2023-02-26 05:34:12 +0300

    done html mobile version

--- main/script.js
+++ main/script.js
@@ -39,3 +39,45 @@ function addBulletPoint() {
   var textarea = document.getElementById("text");
   textarea.value += "\n• ";
 }
+
+function openUrlPopup_1() {
+    var url = prompt("Введите URL-адрес");
+    if (url) {
+      var textArea = document.getElementById("text_1");
+      textArea.value += url;
+    }
+  }
+
+function toggleBold_1() {
+    var textBox = document.getElementById("text_1");
+    if (textBox.style.fontWeight === "bold") {
+        textBox.style.fontWeight = "normal";
+    } else {
+        textBox.style.fontWeight = "bold";
+    }
+}
+
+function toggleUnderline_1() {
+  var input = document.getElementById("text_1");
+  var currentStyle = input.style.textDecoration;
+  if (currentStyle === "underline") {
+    input.style.textDecoration = "none";
+  } else {
+    input.style.textDecoration = "underline";
+  }
+}
+
+function toggleItalic_1() {
+  var input = document.getElementById("text_1");
+  var currentStyle = input.style.fontStyle;
+  if (currentStyle === "italic") {
+    input.style.fontStyle = "normal";
+  } else {
+    input.style.fontStyle = "italic";
+  }
+}
+
+function addBulletPoint_1() {
+  var textarea = document.getElementById("text_1");
+  textarea.value += "\n• ";
+}
